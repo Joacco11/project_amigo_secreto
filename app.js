c8802a458dffe2120e9b6275f14362b406ea2be9
Joacco11 <jahb1102@gmail.com>
2025-02-19 11:14:08 -0500

    archivos iniciales

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+
+
+function agregarAmigo() {
+    nombreAmigo=document.getElementById("amigo").value;
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+    listaAmigos.push(nombreAmigo);
+    input.value = "";
+}
